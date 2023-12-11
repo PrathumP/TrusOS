6e15541aa99eed14df6d8de02cfb27f4b14984eb
PrathumP <115390367+PrathumP@users.noreply.github.com>
2023-12-11 17:05:46 +0530

    Kernel

---
+++ src/home.c
@@ -0,0 +1,15 @@
+#include "../include/home.h"
+//#include "../include/tty.h"
+
+void print_logo()
+{
+    printk("\t                                                         \n");
+    printk("\t  ________                                ____    _____ \n");
+    printk("\t |__    __|                     / __ \\  / ____|\n");
+    printk("\t    |  |   ____  __   _  ___  | |  | || (___  \n");
+    printk("\t    |  |  |  __||  | | | |/ __| | |  | | \\___ \\ \n");
+    printk("\t    |  |  | |   |  |_| | \\__\\ | |__| | ____) |\n");
+    printk("\t    |__|  |_|    \\__,_| |___/  \\____/ |_____/ \n");
+    printk("\t                                                         \n");
+    printk("\t                                                         \n");
+}
